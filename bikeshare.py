295dcb2fcb6014ac90a933658f7766b9f9ce11af
unknown <sosta9918@yahoo.com>
2021-09-13 17:45:04 +0200

    refactor: Refactored a method name so that its name describes better what the function does

--- bikeshare.py
+++ bikeshare.py
@@ -83,7 +83,7 @@ def get_filters():
     return city, month, day
 
 
-def load_data(city, month, day):
+def import_city_data(city, month, day):
     """
     Loads data for the specified city and filters by month and day if applicable.
 
@@ -232,7 +232,7 @@ def get_raw_data(df):
 def main():
     while True:
         city, month, day = get_filters()
-        df = load_data(city, month, day)
+        df = import_city_data(city, month, day)
 
         time_stats(df)
         station_stats(df)
